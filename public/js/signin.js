c08f350c44c981b0fdeb75c50d56d53a7bc05ea0
Evans Owusu Ofori <vansofori07@yahoo.com>
2022-10-05 21:55:50 +0000

    signin finished

---
+++ public/js/signin.js
@@ -0,0 +1,45 @@
+
+// Login user check
+async function signIn() {
+    sessionStorage.clear;
+    let uname = document.querySelector("#username").value;
+    let pwd = document.querySelector("#password").value;
+    if (uname == '') {
+        alert("please enter username.");
+    }
+    else if (pwd == '') {
+        alert("enter the password.");
+    }
+    else if (pwd.length < 5) {
+        alert("Password min length is 5.");
+    }
+    else {
+        const payload = {
+            "username": uname,
+            "password": pwd
+        }
+        const response = await fetch('https://freddy.codesubmit.io/login', {
+            headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+            method: 'POST',
+            body: JSON.stringify(payload)
+        });
+        const data = await response.json();
+        sessionStorage.setItem("access_token", data.access_token);
+        sessionStorage.setItem("refresh_token", data.refresh_token);
+        console.log(data);
+        if (sessionStorage.getItem("access_token")) {
+            // alert('Login Successful Redirecting...');
+            //Redirecting to dashboard.
+            window.location = "../dashboard/index.html";
+            window.location.href(`${window.location}`);
+
+            // window.open('file:/dashboard/index.html');
+            return false;
+        } else if (data.msg) {
+            alert("Wrong Username or Password");
+        }
+
+    }
+}
+const login = document.querySelector("#login");
+login.addEventListener('click', signIn);
