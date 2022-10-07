ba95325c87cadb90683e70a1bcb23d1adf4d3234
Evans Owusu Ofori <vansofori07@yahoo.com>
2022-10-07 06:07:42 +0000

    Done

--- public/js/orders.js
+++ public/js/orders.js
@@ -1,8 +1,14 @@
-let data, table, sortCol;
-let sortAsc = false;
-const pageSize = 3;
-let curPage = 1;
 
+let current_page = 1;
+const records_per_page = 10;
+let totalPages = 0;
+let intDATA = null;
+
+let listing_table = document.querySelector('#catTable tbody');
+const btn_next = document.getElementById("btn_next");
+const btn_prev = document.getElementById("btn_prev");
+let page_at = document.getElementById("page_at");
+let page_total = document.getElementById("page_total");
 
 async function getData(link, token_type, Method) {
     const response = await fetch(`https://freddy.codesubmit.io/${link}`, {
@@ -12,29 +18,59 @@ async function getData(link, token_type, Method) {
     return await response.json();
 }
 // TABLE LOGIC 
-function renderTable(block) {
+function renderTable(block, page) {
     // create html
     let result = '';
-    block.forEach(row => {
-        result += `<tr>
-     <td>${row.product["name"]}</td>
-     <td>${row.created_at}</td>
-     <td>${row.total}</td>
-     <td>${row.status}</td>
-     </tr>`;
-    });
+    if (block != null) {
+        block.forEach(row => {
+            result += `<tr>
+            <td>${row.product["name"]}</td>
+            <td>${row.created_at}</td>
+            <td>${row.total}</td>
+            <td>${row.status}</td>
+            </tr>`;
+        });
+    } else {
+        for (let i = (page - 1) * records_per_page; i < (page * records_per_page); i++) {
+            result += `<tr>
+                    <td>${intDATA[i].product["name"]}</td>
+                    <td>${intDATA[i].created_at}</td>
+                    <td>${intDATA[i].total}</td>
+                    <td>${intDATA[i].status}</td>
+                    </tr>`;
+        }
+    }
     
-    document.querySelector('#catTable tbody').innerHTML = result;
+    listing_table.innerHTML = result;
+
+    page_at.innerHTML = current_page;
+    page_total.innerHTML = totalPages;
+
+    if (current_page == 1) {
+        btn_prev.style.visibility = "hidden";
+    } else {
+        btn_prev.style.visibility = "visible";
+    }
+
+    if (current_page == totalPages) {
+        btn_next.style.visibility = "hidden";
+    } else {
+        btn_next.style.visibility = "visible";
+    }
 }   
 
 window.onload = async function () {
+    
+
     if (sessionStorage.getItem("access_token")) {
-        const DATA = await getData(`orders?page=${curPage}&q=${search}`, "access_token", "GET");
-        console.log(DATA.orders);
-        if (DATA.msg) {
-            alert(DATA.msg);
+        intDATA = await getData(`orders?page=${current_page}&q=${search}`, "access_token", "GET");
+        console.log(intDATA.orders);
+        if (intDATA.msg) {
+            alert(intDATA.msg);
         } else {
-            renderTable(DATA.orders); 
+            totalPages = numPages(intDATA.orders);;
+            console.log(totalPages);
+            renderTable(intDATA.orders, current_page); 
         }
         setInterval(async function () {
             newToken = await getData("refresh", "refresh_token", "POST");
@@ -42,7 +78,7 @@ window.onload = async function () {
             if (newToken.msg) {
                 alert(newToken.msg);
             } else {
-                sessionStorage.setItem("access_token", data.access_token);
+                sessionStorage.setItem("access_token", newToken.access_token);
             }
         }, 900000);
     } else {
@@ -51,40 +87,35 @@ window.onload = async function () {
     }
 }
 async function previousPage() {
-    if (curPage > 1) curPage--;
-    const prevData = await getData(`orders?page=${curPage}`, "access_token", "GET");
-    console.log(prevData);
-    if (prevData.msg) {
-        alert(prevData.msg);
-    } else {
-        renderTable(prevData.orders);
+    if (current_page > 1) { 
+        current_page--;
+        renderTable(null, current_page);
     }
 }
 
 async function nextPage() {
-    if ((curPage * pageSize) < data.length) curPage++;
-    const nextData = await getData(`orders?page=${curPage}`, "access_token", "GET");
-    console.log(nextData);
-    if (nextData.msg) {
-        alert(nextData.msg);
-    } else {
-        renderTable(nextData.orders);
+    if ((current_page * records_per_page) < data.length) {
+        current_page++;
+        renderTable(null, current_page);
     }
 }
 async function search(srchParam) {
-    const searchData = await getData(`orders?page=${curPage}&q=${srchParam.value}`, "access_token", "GET");
+    
+    const searchData = await getData(`orders?page=${current_page}&q=${srchParam.value}`, "access_token", "GET");
     console.log(searchData);
     if (searchData.msg) {
         alert(searchData.msg);
     } else {
-        renderTable(searchData.orders);
+        totalPages = numPages(searchData.orders);
+        renderTable(searchData.orders, current_page);
     }
 }
 
 
+function numPages(DATA) {
+    return Math.ceil(DATA.length / records_per_page);
+}
 
-document.querySelector('#nextButton').addEventListener('click', nextPage, false);
-document.querySelector('#prevButton').addEventListener('click', previousPage, false);
 
 function logout() {
     sessionStorage.clear;
